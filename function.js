d6db899fd32be248e543eabc033d9e16d065c684
MateTheBrogrammer <137646488+MateTheBrogrammer@users.noreply.github.com>
2023-06-24 22:28:09 +0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+
+document.getElementById("generateNumber").onclick = function() {
+    var minNumber = parseInt(document.getElementById("minimumNumber").value);
+    var maxNumber = parseInt(document.getElementById("maximumNumber").value);
+    var generatedNumber = document.getElementById("generatedNumber");
+    
+    let value = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    generatedNumber.textContent = value;
+}
